b367cb0de6820b407fbe0d38e43405e6c064a37b
collinsbj <collinsbj@me.com>
2018-01-21 19:33:26 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./App.css";
+import Laptops from "./Laptops";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      laptops: [],
+      phones: []
+    };
+  }
+
+  componentDidMount() {
+    var laptops = [];
+    var phones = [];
+    fetch("https://www.galvanize-tower-back-end.herokuapp.com/laptops")
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => console.log(err));
+    // fetch("https://www.galvanize-tower-back-end.herokuapp.com/phones")
+    //   .then(response => response.json())
+    //   .then(response => {
+    //     console.log(response);
+    //   })
+    //   .catch(err => console.log(err));
+  }
+
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
---
+++ src/Laptops.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import LaptopCards from "./LaptopCards";
+
+function Laptops(props) {
+  console.log(props.laptops);
+}
+
+export default Laptops;
