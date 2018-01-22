05049959bc7121b323f374d17864e38323185e74
collinsbj <collinsbj@me.com>
2018-01-21 19:39:19 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Laptops from "./Laptops";
+// import Laptops from "./Laptops";
 
 class App extends Component {
   constructor(props) {
